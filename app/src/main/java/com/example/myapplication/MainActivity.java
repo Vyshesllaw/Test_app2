2c7f0a82afda0b31b3f0c71e97d1a8d372480c02
Vyacheslav Popov <vyshesllaw@gmail.com>
2023-04-15 12:20:42 +0300

    Создал новую переменную

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private String s = "test";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
